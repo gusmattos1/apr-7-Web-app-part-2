3905481e8db5939603cd918cf3a2d250cf7ba81f
gusmattos1 <gusmattos@gmail.com>
2018-04-09 18:55:05 -0400

    Assignment 3 start

---
+++ assignment3/crm.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require_relative 'contact'
+
+get '/' do
+  redirect to ('/home')
+end
+
+get '/home' do
+  @title = 'Home'
+  erb :index
+end
+
+
+get '/contacts' do
+  @title = 'Contacts'
+
+  @contacts = Contact.all
+  erb :contacts
+end
+
+get '/about' do
+  @title = 'about'
+
+  erb :about
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
